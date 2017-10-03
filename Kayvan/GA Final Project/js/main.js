b19ed8952a9fc0f0e258d6b344ebe2a4c9201827
KayvanCM <30817019+KayvanCM@users.noreply.github.com>
2017-10-02 18:12:58 -0700

    Add files via upload

--- Kayvan/GA Final Project/js/main.js
+++ Kayvan/GA Final Project/js/main.js
@@ -1,18 +1,16 @@
 // Make it rain!
 $(window).on('scroll', function () {
-	// Step 1: Google $(window).scrollTop();
+	
 	var distanceScrolled = $(window).scrollTop();
 
 	console.log('The distance scrolled is: ' + distanceScrolled);
 
-	// Step 2: Write Rest of JS here
-	// when distance scrolled = 545 then add class .sticky to nav
 
 	if (distanceScrolled >= 400) {
-		$(".nav").addClass("scrolled");
+		$("nav").addClass("scrolled");
 	}
 	if (distanceScrolled < 400) {
-		$(".nav").removeClass("scrolled");
+		$("nav").removeClass("scrolled");
 	}
 
 
